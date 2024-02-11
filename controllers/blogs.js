3849917ccfd7301f486a710866447707e7febda5
Margarita <margaritaalcarazrojas@gmail.com>
2024-02-10 22:33:10 -0700

    add login endpoint and link blog to a user - Exercise 13.10

--- controllers/blogs.js
+++ controllers/blogs.js
@@ -1,19 +1,38 @@
 require('express-async-errors')
+const jwt = require('jsonwebtoken')
 const express = require('express')
-const { Blog } = require('../models')
+const { Blog, User } = require('../models')
 const router = express.Router()
+
+const { SECRET } = require('../utils/config')
 
 router.get('/', async (req, res) => {
     const blogs = await Blog.findAll()
     res.json(blogs)
 })
 
-router.post('/', async (req, res) => {
-    const blog = await Blog.create(req.body)
+const tokenExtractor = async (req, res, next) => {
+    const authorization = req.get('authorization')
+    if (authorization && authorization.toLowerCase().startsWith('bearer ')) {
+        try {
+            req.decodedToken = jwt.verify(authorization.substring(7), SECRET)
+        } catch (error) {
+            return res.status(401).json({ error: 'token invalid' })
+        }
+    } else {
+        return res.status(401).json({ error: 'token missing' })
+    }
+    next()
+}
+
+router.post('/', tokenExtractor, async (req, res) => {
+    const user = await User.findByPk(req.decodedToken.id)
+    console.log(JSON.stringify(user), null, 2)
+    const blog = await Blog.create({ ...req.body, UserId: user.id })
     res.json(blog)
 })
 
-router.delete('/:id', async (req, res) => {
+router.delete('/:id', tokenExtractor, async (req, res) => {
     const delete_rows = await Blog.destroy({ where: { id: Number.parseInt(req.params.id) } })
     if (delete_rows == 1) {
         res.status(204).end()
@@ -27,7 +46,7 @@ const blogFinder = async (req, res, next) => {
     next()
 }
 
-router.put('/:id', blogFinder, async (req, res) => {
+router.put('/:id', blogFinder, tokenExtractor, async (req, res) => {
     if (req.blog) {
         req.blog.likes = Number.parseInt(req.body.likes)
         await req.blog.save()
--- models/index.js
+++ models/index.js
@@ -1,6 +1,8 @@
 const Blog = require('./blog')
 const User = require('./user')
 
+User.hasMany(Blog)
+Blog.belongsTo(User)
 User.sync()
 Blog.sync()
 
