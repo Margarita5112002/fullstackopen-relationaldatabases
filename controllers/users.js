acc284e1b96f1366f4d4f62ed57b1e7320ac6287
Margarita <margaritaalcarazrojas@gmail.com>
2024-02-10 21:32:16 -0700

    add users to database - Exercise 13.8

---
+++ controllers/users.js
@@ -0,0 +1,34 @@
+const express = require('express')
+const { User } = require('../models')
+
+const router = express.Router()
+
+router.post('/', async (req, res) => {
+    const user = await User.create(req.body)
+    res.status(201).json(user)
+})
+
+router.get('/', async (req, res) => {
+    const users = await User.findAll()
+    res.json(users)
+})
+
+router.put('/:username', async (req, res) => {
+    const user = await User.findOne({
+        where: { username: req.params.username }
+    })
+    if (user) {
+        if (req.body.name) {
+            user.name = req.body.name
+        }
+        if (req.body.username) {
+            user.username = req.body.username
+        }
+        await user.save()
+        res.json(user)
+    } else {
+        res.status(404).end()
+    }
+})
+
+module.exports = router
